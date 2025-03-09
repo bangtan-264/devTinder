b061d51d81f5a15120bb83a2264a7dd17b09b668
HP <sahilverma2642@gmail.com>
2025-03-09 08:54:56 -0700

    Explored get, post, put, patch, delete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Hello");
+//This will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({firstName: "Sahil Verma", lastName: "Verma", age: "22"});
+});
+
+app.post("/user", (req, res) => {
+    res.send("User data saved successfully");
+});
+
+app.put("/user", (req, res) => {
+    res.send("User data updated successfully");
+});
+
+app.patch("/user", (req, res) => {
+    res.send("User age updated successfully");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("user deleted successfully");
+});
+
+//This will match all the HTTP method api call to /test
+app.use("/test", (req, res) => {
+    res.send("Hello from Mr. Test-ick-llsss");
 });
 
 app.listen('7777', () => {
